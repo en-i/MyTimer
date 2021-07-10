307ec887496f3761554d43f309a6ad6ae990b76b
en-i <templebond@icloud.com>
2021-07-10 20:58:21 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by terada enishi on 2021/07/10.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
